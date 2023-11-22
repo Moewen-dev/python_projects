3d8ac67db0ae9cea7cdd74eaa42d888d7c944242
Moewen-dev <tom.penning@pm.me>
2023-11-22 07:59:33 +0100

    added Todos´s

--- Todo-List-GUI/Todo-List.py
+++ Todo-List-GUI/Todo-List.py
@@ -8,11 +8,29 @@ import PySimpleGUI as sg
 
 sg.theme('DarkAmber')
 
+# Window layout
+print("TODO: Get the layout right")
 layout = [
     [sg.Text('Test')],
     [sg.Input(key='-IN-')],
     [sg.Button('Read'), sg.Exit()]
 ]
+
+# Add task to tasks list
+def add_task(task):
+    print("TODO: add_task(task)")
+
+# Remove task from tasks list
+def remove_task(taskID):
+    print("TODO: remove_task(taskID)")
+
+# Save tasks to binary file
+def save_tasks(tasks):
+    print("TODO: save_tasks(tasks)")
+
+# Load tasks from binary file
+def load_tasks():
+    print("TODO: load_tasks()")
 
 window = sg.Window('Window that stays open', layout)
 
